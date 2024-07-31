c2dfc879014491a7750a50ed39c30ca11e9db6ce
firstPush <adel.sdet2@gmail.com>
2024-07-31 15:21:06 -0500

    cc

--- data-structures-and-algorithms/week-14/SortColors.java
+++ data-structures-and-algorithms/week-14/SortColors.java
@@ -1,28 +1,30 @@
 public class SortColors {
     public void sortcolors(int[] nums){
-        int low = 0;
-        int med = 0;
-        int high = nums.length-1;
+   int low = 0;
+   int mid = 0;
+   int high = nums.length-1;
 
-        while(med<= high){
-            if(nums[med] == 0){
-                int temp = nums[low];
-                nums[low] = nums[med];
-                low++;
-                med++;
+   while(mid<=high){
+    if(nums[mid] == 0){
+        int temp  = nums[low];
+        nums[low] = nums[mid];
+        nums[mid] = temp;
+        low++;
+        mid++;
 
+    }else if(nums[mid] == 1){
+        mid++;
 
-            }else if(nums[med] == 1){
-                med++;
-                
-            }else if(nums[med] == 2){
-                int temp = nums[med];
-                nums[med] = nums[high];
-                high--;
-
-            }
-        }
+    }else if(nums[mid] == 2){
+        int temp = nums[mid];
+        nums[mid] = nums[high];
+        nums[high] = temp;
+        high--;
+        
 
     }
-    
+   }
+
+
+    }
 }
