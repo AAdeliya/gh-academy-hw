66eafa42af4754464e2d4f93d942c65979b437cd
firstPush <adel.sdet2@gmail.com>
2024-08-01 11:47:38 -0400

    rotaty

--- data-structures-and-algorithms/week-14/RotateArray.java
+++ data-structures-and-algorithms/week-14/RotateArray.java
@@ -2,26 +2,28 @@
 
 public class RotateArray {
 public void rotate(int[] nums, int k){
-    k %=nums.length;
+    k%=nums.length;
     reverse(nums, 0, nums.length-1);
     reverse(nums, 0, k-1);
     reverse(nums, k, nums.length-1);
 
-    
 }
-
-public void reverse(int[]nums, int start, int end){
+public void reverse(int[] nums, int start, int end){
     while(start<end){
         int temp = nums[start];
         nums[start] = nums[end];
-        nums[end]= temp;
+        nums[end] = temp;
         start++;
-        end++;
-        
+        end--;
 
     }
 }
+
+
+    
 }
+
+
 //k=3 i shows the number of rotate 
 // [1,2,3,4,5,6,7]
 //1. [7,1,2,3,4,5,6]
