97dd675c3b1ac7baa8bcfa2891b8c2a36b4fcd82
firstPush <adel.sdet2@gmail.com>
2024-08-01 15:43:49 -0400

    merge

--- data-structures-and-algorithms/week-14/MergeArrayByParity.java
+++ data-structures-and-algorithms/week-14/MergeArrayByParity.java
@@ -1,18 +1,32 @@
 public class MergeArrayByParity {
-    public void merge(int[] nums1, int m, int[] nums2, int n){
-        int i = m-1;
-        int j = n-1;
-        int k=m+n-1;
+    public void merge(int[] nums1, int p1, int[] nums2, int p2){
+        int i = p1-1;
+        int j = p2-1;
+        int k=p1+p2-1;
 
-        while(j>=0){
-            if(i>=0 && nums1[i] > nums2[j]){
-                nums1[k--] = nums1[j--];
+        while(nums1[i]>=0 && nums2[j]>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k] = nums1[i];
+                i--;
+                j--;
 
-            }else{
-                nums1[k--] = nums2[j--];
-                
+
+            }else {
+                nums1[k] = nums2[j];
+                j--;
+                k--;
+
             }
+
         }
+        while(j>=0){
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+            
+        }
+
+        
 
     }
 
