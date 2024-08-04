a7437b506b861559072c4386f73d374e67097ec8
firstPush <adel.sdet2@gmail.com>
2024-08-04 13:39:45 -0400

    first bad version

---
+++ data-structures-and-algorithms/week-17/BinarySearch.java
@@ -0,0 +1,23 @@
+
+// public class BinarySearch {
+//     int left = 0;
+//     int right = n-1;
+
+//     while(left<=right){
+//         int middle = (left+right)/2;
+//         if(nums[middle] == x){
+//             return middle;
+
+//         }
+//         if(nums[middle] <x){
+//             left = middle+1;
+
+//         }else{
+//             right = middle-1;
+
+//         }
+//     }
+//     return -1;
+//     //couldn't findit
+
+// }
---
+++ data-structures-and-algorithms/week-17/FirstBadVersion.java
@@ -0,0 +1,21 @@
+
+public class FirstBadVersion extends VersionControl{
+    public int FirstBadVersion(int n){
+        int left= 0;
+        int right = n;
+        int answer = -1;
+
+        while(left<=right){
+            int middle = left + (right+left)/2;
+            if(!isBadVersion(middle)){
+                left = middle+1;
+
+            }else{
+                answer = middle;
+                right = middle-1;
+
+            }
+        }
+        return answer;
+    }
+}
