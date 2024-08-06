971e9aae3a21d492c9392489ad3316565fe638d7
firstPush <adel.sdet2@gmail.com>
2024-08-05 21:17:46 -0500

    4

---
+++ data-structures-and-algorithms/week-17/CapacityToShipPackagesWithinDdays.java
@@ -0,0 +1,16 @@
+public class CapacityToShipPackagesWithinDdays {
+    public int shipWithinDays(int[] weights, int D) {
+       int left = 0;
+       int right = 0;
+       int answer = -1;
+
+        while (left<= right) {
+            int middle = (left + right) / 2;
+            
+
+
+        }
+        return answer;
+    }
+    
+}
