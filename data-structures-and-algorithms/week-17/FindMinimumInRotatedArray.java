9b0a8d9854455bb37c7b21a97fa975dfb6462eab
firstPush <adel.sdet2@gmail.com>
2024-08-06 19:41:22 -0500

    find min

---
+++ data-structures-and-algorithms/week-17/FindMinimumInRotatedArray.java
@@ -0,0 +1,34 @@
+public class FindMinimumInRotatedArray {
+    public int findMin(int[] nums) {
+        int n = nums.length;
+
+        if(nums == null || n == 0){
+
+        if(n == 1) return nums[0];
+
+        if(nums[0] < nums[n-1]) return nums[0];
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left<=right) {
+            int middle = (left+right)/2;
+
+        if(middle > 0 && nums[middle - 1] > nums[middle]) return nums[middle];
+
+        
+
+        if(nums[middle] > nums[right]) {
+            left = middle+1;
+
+        } else {
+            right = middle-1;
+        }
+        }
+       
+    }
+        return -1;
+    }
+}
+    
+
