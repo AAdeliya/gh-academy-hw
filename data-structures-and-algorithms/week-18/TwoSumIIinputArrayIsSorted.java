5d1ee3f3e486860258762d7d82e895fdd2d4f7fe
firstPush <adel.sdet2@gmail.com>
2024-08-08 11:27:24 -0500

    twoSum

---
+++ data-structures-and-algorithms/week-18/TwoSumIIinputArrayIsSorted.java
@@ -0,0 +1,26 @@
+
+
+public class TwoSumIIinputArrayIsSorted {
+    public int[] twoSum(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+
+        while (left<right) {
+            int total = nums[left] + nums[right];
+
+            if (total == target) {
+                return new int[] {left+1, right+1};
+
+            } else if(total> target) {
+                right--;
+
+            } else {
+                left++;
+
+
+            }
+        }
+        return new int[] {-1, -1};
+    }
+    
+}
