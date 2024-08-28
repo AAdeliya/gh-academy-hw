4a2a19f920177e564d2e894bb6c9b91d78eb297b
firstPush <adel.sdet2@gmail.com>
2024-08-28 09:01:04 -0500

    code

--- data-structures-and-algorithms/week-14/LongestCommonPrefix.java
+++ data-structures-and-algorithms/week-14/LongestCommonPrefix.java
@@ -1,9 +1,9 @@
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] str){
-        if(str.length == 0) return "";
+        if (str.length == 0) return "";
         String prefix = str[0];
-        for(int i =1; i<str.length; i++){
-            while(str[i].indexOf(prefix) !=0){
+        for (int i =1; i<str.length; i++) {
+            while (str[i].indexOf(prefix) !=0) {
                 prefix = prefix.substring(0, prefix.length()-1);
 
             }
