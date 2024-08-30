0f275b61bcabeb4e6a73c324e458f7d1d7fa51e5
firstPush <adel.sdet2@gmail.com>
2024-08-30 17:48:34 -0500

    chips

---
+++ data-structures-and-algorithms/week-19/Chips.java
@@ -0,0 +1,24 @@
+public class Chips {
+    public static List<Integer> solveChipsProblem(int n) {
+        List<Ineger> actions = new ArrayList<>();
+
+        for(int = 1; i <=N; i++) {
+            actions.add(i);
+        }
+        for (int i= N; i>= 1; i--) {
+            actions.add(-i);
+
+        }
+        return actions;
+    }
+    public static void main(String[] args) {
+        int N =3;
+        List<Integer> result = solveChipsProblem(N);
+        
+        for (int action : result) {
+            System.out.println(action + " ");
+            
+        }
+    }
+    
+}
