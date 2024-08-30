fa6465cb16631fe46ca344ba0e55bd33f9c31894
firstPush <adel.sdet2@gmail.com>
2024-08-30 16:20:54 -0500

    tower of hanoi

--- data-structures-and-algorithms/week-19/TowerOfHanoi.java
+++ data-structures-and-algorithms/week-19/TowerOfHanoi.java
@@ -1,20 +1,17 @@
 import java.io.PrintWriter;
 
-// public class TowerOfHanoi {
-//     public class Hanoi {
-//     private static class Solution {
-//         private void hanoi(int n, int from, int to) {
-//             if (n == 0) return;
-//             int aux = (1 + 2 + 3) - (from + to);
-//             hanoi(n - 1, from, aux);
-//             System.out.println(n + " " + from + " " + to);
-//             hanoi(n - 1, aux, to);
-//         }
+public class TowerOfHanoi {
+    
+    private static class Solution {
+        private void hanoi(int n, int from, int to) {
+            if (n == 0) return;
+            int aux = (1 + 2 + 3) - (from + to);
+            hanoi(n - 1, from, aux);
+            System.out.println(n + " " + from + " " + to);
+            hanoi(n - 1, aux, to);
+        }
  
-//         public void run(InputReader in, PrintWriter out) {
-//             int n = in.nextInt();
-//             hanoi(n, 1, 3);
-//         }
-//     }
-//
+
+    }
+}
     
