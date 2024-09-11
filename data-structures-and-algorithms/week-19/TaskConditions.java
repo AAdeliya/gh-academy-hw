166d8b5231b53cbfa96c8ce3cbe38a09b4b82d7c
firstPush <adel.sdet2@gmail.com>
2024-09-10 22:06:27 -0500

    tasks cond

--- data-structures-and-algorithms/week-19/TaskConditions.java
+++ data-structures-and-algorithms/week-19/TaskConditions.java
@@ -1,4 +1,33 @@
 public class TaskConditions {
+    private InputReader in_;
+    private PrinterWriter out_;
+
+    public void put(int n) {
+        if (n == 1) {
+            out_.print("1 ");
+            return;
+
+
+        } 
+
+        put(n-1);
+        out_.print(n + "");
+        remove(n-1);
+
+
+    }
+    private void remove(int n) {
+        if (n==1) {
+            out_.print("-1");
+            return;
+
+        }
+
+        put(n-1);
+        out_.print(-n+ "");
+        remove(n-1);
+         
+    }
 
     
     
