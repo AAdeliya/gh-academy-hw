0ae4cb30fef2e4a5c08069244bb987162bc2096e
firstPush <adel.sdet2@gmail.com>
2024-09-25 00:02:31 -0500

    recursive linked list

---
+++ data-structures-and-algorithms/week-21/ReverseLinkedList.java
@@ -0,0 +1,33 @@
+import java.lang.classfile.components.ClassPrinter.ListNode;
+
+public class ReverseLinkedList {
+    // public static void main(String[] args) {
+        
+    // }
+
+    public ListNode reverseList(ListNode head) {
+    ListNode prev = null;
+    ListNode next = null;
+    ListNode cur = head;
+
+    while (cur != null) {
+        next = cur.next; //save the next node
+        cur.next = prev; //reverse the link
+        prev = cur; ///move prev to cur
+        cur = next; //mode cur to the next ode (which was seved earclier)
+
+    }
+    return prev; //return the new head of the reversed list 
+}
+}
+
+// public ListNode reverseList(ListNode head) {
+//     retutn reverseListRecursive(head, null);
+// }
+// //recursion approach 
+// public ListNode reverseList(ListNode currentNode, ListNode newNext ) {
+//     i (currentNode == null ) return newNext;
+//     ListNode oldNext  = currentNode.next;
+//     currentNode.nezt = newNext;
+//     return reverseList(oldNext, currentNode);
+
