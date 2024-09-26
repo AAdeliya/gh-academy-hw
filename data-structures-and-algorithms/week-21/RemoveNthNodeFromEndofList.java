b2183c30028a6c39a599868778589c461037af32
firstPush <adel.sdet2@gmail.com>
2024-09-25 20:20:26 -0500

    remove Nth element from linked list

---
+++ data-structures-and-algorithms/week-21/RemoveNthNodeFromEndofList.java
@@ -0,0 +1,26 @@
+
+
+public class RemoveNthNodeFromEndofList {
+    public ListNode removeNthElement (ListNode head, int n) {
+        ListNode fast = head, slow = head;
+       for (int i = 0; i<n; i++) {
+        fast = fast.next;
+
+       }
+
+       if (fast == null) {
+        return head.next;
+       }
+
+       while (fast.next != null) {
+        fast = fast.next;
+        slow = slow.next;
+
+       }
+
+       slow.next = slow.next.next;
+       return head;
+
+    }
+    
+}
