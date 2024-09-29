c473f431aea85d0f4233854f33943953d8a4c419
firstPush <adel.sdet2@gmail.com>
2024-09-29 16:19:19 -0500

    week 22 valid parenthesis

--- data-structures-and-algorithms/week-21/DesignBrowserHistory.java
+++ data-structures-and-algorithms/week-21/DesignBrowserHistory.java
@@ -1,42 +1,44 @@
 public class DesignBrowserHistory {
-    public class Node {
-        String url;
-        Node prev, next;
+public class Node {
+    String url;
+    Node prev, next;
 
-        public Node(String url) {
-            this.url = url;
-            this.prev= null;
-            this.next = null;
+    public Node(String url) {
+        this.url = url;
+        this.prev = null;
+        this.next  = null;
 
-        }
+    }
+}
+
+    public Node current;
+
+    public DesignBrowserHistory (String homepage) {
+        current = new Node(homepage);
     }
 
-        public Node current;
+    public String visit (String url) {
+        Node newUrl = new Node(url);
+        current.next = newUrl;
+        newUrl.prev  =current;
+        current = newUrl;
+        return url;
+     }
 
-        public DesignBrowserHistory(String homepage) {
-            current = new Node(homepage);
+    public String back (int steps) {
+        for (; current.prev !=null && steps>0; steps--) {
+            current = current.prev;
+        }
+        return current.url;
     }
 
-        public String visit(String url) {
-            Node newUrl = new Node(url);
-            current.next = newUrl;
-            current = newUrl;
+    public String forward(int steps) {
+        for (; current.next != null && steps>0; steps++) {
+            current  = current.next;
         }
 
-        public String back(int steps) {
-            for (; current.prev != null && steps>0; steps--) {
-                current = current.prev;
-            }
-
-            return current.url;
-        }
-
-        public String forward(int steps) {
-            for (; current.next != null && steps>0; steps--) {
-                current  = current.next;
-
-            }
-
-            return current.url;
-        }
+        return current.url;
     }
+    
+}
+
