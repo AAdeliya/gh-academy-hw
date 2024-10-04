c23a1bf962ce32e55fda76af5cdd0e03af0aac8f
firstPush <adel.sdet2@gmail.com>
2024-10-03 23:44:50 -0500

    Implement Queue using Stacks

---
+++ data-structures-and-algorithms/week -23/ImplementQByUsingStack.java
@@ -0,0 +1,32 @@
+
+
+public class ImplementQByUsingStack {
+    Stack<Integer> input = new Stack<>();
+    Stack<Integer> output = new Stack<>();
+
+
+    public void push() {
+        input.push(x);
+
+
+    }
+
+    public int pop() {
+        int x =  peek();
+        output.pop();
+        return x;
+    }
+
+    public int peek() {
+        if (output.empty()) {
+            while (!input.empty())
+            output.push(input.pop());
+            return output.peek();
+        }
+
+        public boolean empty() {
+            return input.empty() && output.empty();
+        }
+    }
+    
+}
