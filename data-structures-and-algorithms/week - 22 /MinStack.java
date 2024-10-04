00d69be54c256407a8980e117e6213bb7073d8cf
firstPush <adel.sdet2@gmail.com>
2024-10-03 22:31:05 -0500

    min stack

--- data-structures-and-algorithms/week - 22 /MinStack.java
+++ data-structures-and-algorithms/week - 22 /MinStack.java
@@ -1,5 +1,42 @@
 
 
 public class MinStack {
+    private Node head;
+
+    public void push(int x) {
+        if (head == null) {
+            head = new Node(x, x, null);
+
+        } else {
+            head = new Node(x, Math.min(x, head.min), x);
+
+        }
+    }
+
+    public int pop() {
+        return head.next;
+    }
+    
+    public void pop() {
+        return head.val;
+    }
+    
+    public int min() {
+        return head.min;
+    }
+
+    private class Node {
+        int val;
+        int min;
+        Node next;
+
+        public Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+
+
     
 }
