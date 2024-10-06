d7693a5890572a683925a567de4b3390623f9cc9
firstPush <adel.sdet2@gmail.com>
2024-10-06 17:53:49 -0500

    week 23 - 3 in progress

---
+++ data-structures-and-algorithms/week -23/DesignCircularDeque.java
@@ -0,0 +1,16 @@
+public class DesignCircularDeque {
+    
+    class MyCircularDeque {
+        class Node {
+            public int val;
+            public Node next;
+            public Node prev;
+    
+            public Node(int val, Node next, Node prev) {
+                this.val = val;
+                this.next = next;
+                this.prev = prev;
+            }
+    
+        }
+}
