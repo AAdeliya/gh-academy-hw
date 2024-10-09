557e81f6d6588e2fe57e7c857537620265acd06b
firstPush <adel.sdet2@gmail.com>
2024-10-08 19:40:53 -0500

    week 23 - 3

--- data-structures-and-algorithms/week -23/DesignCircularDeque.java
+++ data-structures-and-algorithms/week -23/DesignCircularDeque.java
@@ -1,23 +1,103 @@
 public class DesignCircularDeque {
     
     class MyCircularDeque {
-        class Node {
-            public int val;
-            public Node next;
-            public Node prev;
-    
-            public Node(int val, Node next, Node prev) {
-                this.val = val;
-                this.next = next;
-                this.prev = prev;
-            }
-    
-        }
-        public MyCircularDeque(int k) {
+        public Node {
             Node head;
             Node rear;
             int size;
-            int cappacity;
+            int capacity;
+
+        public MyCircularDeque(int k) {
+            size = 0;
+            capacity = k;
+         }
+        
+        public boolean insertFront(int value) {
+            if (isFull()) return false;
+            if (head == null) {
+                head = newNode(value, null, null);
+                rear = head;
+
+            } else {
+                Node newHead = new Node(value, head, null)
+                head.prev = newHead;
+                head = newHead;
+            }
+            size++;
+            return true;
             
         }
-}
+
+        
+        public boolean insertLast(int value) {
+            if (isFull()) return false;
+            if (head == null) {
+                head = newNode(value, null, null);
+                rear= head;
+                
+            } else {
+                rear.next= new Node(value, rear, rear)
+                rear = rear.next
+            }
+            size++;
+            return true;
+
+            
+        }
+        
+        public boolean deleteFront() {
+            if (isEmpty()) return false;
+            if (size == 1) {
+                head== null;
+                read == null;
+            } else {
+                head.next = head;
+            }
+            size--;
+            return true;
+
+            }
+
+
+        
+        public boolean deleteLast() {
+            if (isEmpty()) return false;
+            if (size == 1) {
+                head == null;
+                rear == null;
+            } else {
+                rear=rear.prev;
+            }
+            size--;
+            return true;
+
+            
+        }
+        
+        public int getFront() {
+            if (isEmpty()) return -1;
+            return head.val;
+            
+        }
+        
+        public int getRear() {
+            if (isEmpty()) return -1;
+            return rear.val;
+
+            
+        }
+        
+        public boolean isEmpty() {
+            return size == 0;
+        }
+        
+        public boolean isFull() {
+            return size = capacity;
+            
+        }
+    }
+    
+
+
+
+ 
