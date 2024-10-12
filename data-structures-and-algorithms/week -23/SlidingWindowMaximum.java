81b8fc03dc46d31f1d0d70caea6dd16f9ddfb1d5
firstPush <adel.sdet2@gmail.com>
2024-10-12 14:30:45 -0500

    sliding window maximum

---
+++ data-structures-and-algorithms/week -23/SlidingWindowMaximum.java
@@ -0,0 +1,31 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class SlidingWindowMaximum {
+    public int[] maxSlidingWindow(int[] a, int k ) {
+        if (k== null || k<=0) {
+            return new int[0];
+
+    }
+
+    int n = a.length;
+    int[] r = new int[n-k+1];
+    inr ri = 0;
+    Deque<Integer> q = new ArrayDeque<>();
+    for (int i = 0; i< a.length; i++) {
+        while (!q.isEmpty() && q.peekFirst() < i-k+1) {
+            q.poll();
+        }
+
+        while( !q.isEmpty() && a[i] > a[q.peekLast()]) {
+            q.pollLast();
+        }
+
+        q.offer(i);
+        if (i>= k -1) {
+            r[ri++] = a[q.peekFirst()];
+        }
+    }
+    return r;
+    
+}
