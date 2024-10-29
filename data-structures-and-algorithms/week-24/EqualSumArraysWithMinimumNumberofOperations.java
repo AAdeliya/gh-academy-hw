fe0c9e3629f213d6da790bf2faacd26339be1020
firstPush <adel.sdet2@gmail.com>
2024-10-28 21:08:51 -0500

    week 24 EqualSumArraysWithMinimumNumberofOperations

---
+++ data-structures-and-algorithms/week-24/EqualSumArraysWithMinimumNumberofOperations.java
@@ -0,0 +1,8 @@
+package data-structures-and-algorithms.week-24;
+
+public class EqualSumArraysWithMinimumNumberofOperations {
+    public int minOperations(int[] nums1, int[] nums2) {
+        
+    }
+    
+}
