af93ef11aaff31dc04817cc48fc39e37c6596626
firstPush <adel.sdet2@gmail.com>
2024-10-31 21:05:34 -0500

    rotaty array

--- data-structures-and-algorithms/week-14/RotateArray.java
+++ data-structures-and-algorithms/week-14/RotateArray.java
@@ -31,4 +31,21 @@ public void reverse(int[] nums, int start, int end){
 //3.[5,6,7,1,2,3,4]
 
 
-
+class Solution {
+    public void rotate(int[] nums, int k) {
+     k %= nums.length;
+     reverse(nums, 0, nums.length - 1);
+     reverse(nums, 0, k - 1);
+     reverse(nums, k, nums.length - 1);
+ }
+ 
+ public void reverse(int[] nums, int start, int end) {
+     while (start < end) {
+         int temp = nums[start];
+         nums[start] = nums[end];
+         nums[end] = temp;
+         start++;
+         end--;
+     }
+ }
+ }
