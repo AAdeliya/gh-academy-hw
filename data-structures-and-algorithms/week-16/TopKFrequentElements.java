d5ef738f90cb11118f0a71db0d6200c9b52c3722
firstPush <adel.sdet2@gmail.com>
2024-11-10 11:33:51 -0600

    TopKFrequentElements

--- data-structures-and-algorithms/week-16/TopKFrequentElements.java
+++ data-structures-and-algorithms/week-16/TopKFrequentElements.java
@@ -1,18 +1,57 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class TopKFrequentElements {
 
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> freq = new HashMap<>();
-        for (int x : freq) {
-            freq.put(x, freq.getOrDefault(x, 0) + 1);
-        }
+Map<Integer, Integer> freq = new HashMap<>();
+for (int x: nums) {
+    freq.put(x, freq.getOrDefault(x, 0)+ 1);
 
-        List<Pair<Integer, Integer>> sorted_freq_pair = new ArrayList<>();
-        for (var entry : freq.entrySet()) {
-            Pair<Integer, Integer> freq_pair = new Pair<>(entry.getKey(), entry.getValue());
-            sorted_freq_pair.add(freq_pair);
-        }
+}
+
+List<Pair<Integer, Integer>> sorted_freq_pair = new ArrayList<>();
+for (var entry: freq.entrySet()) {
+    Pair<Integer, Integer> freq_pairs=  new Pair<>(entry.getKey(), entry.getValue());
+    sorted_freq_pair.add(freq_pairs);
+
+}
+
+Collections.sorted(sorted_freq_pair, (fp1, fp2) -> {
+    return fp2.getValue() - fp1.getValue();
+});
+
+int[] results = new int[k];
+for (int i = 0; i<k; i++) {
+    results[i] = sorted_freq_pair.get(i).getKey();
+
+}
+
+return results;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         Collections.sort(sorted_freq_pair, (fp1, fp2) -> {
             return fp2.getValue() - fp1.getValue(); });
