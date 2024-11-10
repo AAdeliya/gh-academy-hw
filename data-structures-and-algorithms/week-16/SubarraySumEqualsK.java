a1d5ef92d92c763e30ce4bee885540af87bd666e
firstPush <adel.sdet2@gmail.com>
2024-11-09 20:11:04 -0600

    Subarray Sum Equals K

--- data-structures-and-algorithms/week-16/SubarraySumEqualsK.java
+++ data-structures-and-algorithms/week-16/SubarraySumEqualsK.java
@@ -1,4 +1,33 @@
 public class SubarraySumEqualsK {
     public int subarraySum(int[] nums, int k) {
+        
+
+
+
+
+
+
+
+
+
+    //     public int subarraySum(int[] nums, int k) {
+    //         if (nums.length<=1) {
+    //            return -1;
+       
+    //         }
+    //         int count = 0;
+    //         for (int i = 0; i<nums.length; i++) {
+    //            int currentSum = 0;
+    //                for (int j = i; j < nums.length; j++) {
+    //                    currentSum += nums[j];
+    //                    if (currentSum == k) {
+    //                        count++;
+    //                    }
+    //                }
+    //            }
+    //            return count;
+    //        }
+    //    }
+       
     }
 }
