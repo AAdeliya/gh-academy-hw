75da89e99dac5dd4a89b7ebbe2d9211c5bfbe93f
firstPush <adel.sdet2@gmail.com>
2024-11-09 21:14:40 -0600

    prefixSum

--- data-structures-and-algorithms/week-16/SubarraySumEqualsK.java
+++ data-structures-and-algorithms/week-16/SubarraySumEqualsK.java
@@ -6,14 +6,15 @@ public class SubarraySumEqualsK {
         int answer = 0;
         Map<Integer, Integer> prefixSumSeenCount = new HashMap<>();
         prefixSumSeenCount.put(0,1);
-
         for (int i = 0; i<nums.length; i++) {
             currentPrefixSum+=nums[i];
-            answer+=prefixSumSeenCount.getOrDefault(currentPrefixSum-k, 0);
-            prefixSumSeenCount.put(currentPrefixSum, prefixSumSeenCount.getOrDefault(currentPrefixSum, 0) +1 );
 
+            answer+=prefixSumSeenCount.getOrDefault(currentPrefixSum-k, 0);
+
+            prefixSumSeenCount.put(prefixSumSeenCount.get(currentPrefixSum, 0) +1);
 
         }
+
         return answer;
 
 
