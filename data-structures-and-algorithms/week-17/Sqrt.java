929eee3aef719d677a2f4d91d279fad713fd2f26
firstPush <adel.sdet2@gmail.com>
2024-11-12 21:23:56 -0600

    week 17 - 2 recap

--- data-structures-and-algorithms/week-17/Sqrt.java
+++ data-structures-and-algorithms/week-17/Sqrt.java
@@ -1,29 +1,52 @@
 
 
 public class Sqrt {
-    public int mySqrt(int x){
-        if(x == 0 || x == 1) return x;
+    public int mySqrt(int x) {
+int left = 0;
+int right  = x;
+int answer = -1;
 
-        int left = 1;
-        int right = x;
-        int answer = -1;
+while (left<=right) {
+    int mid = left + (right-left) {
+        if ((long) mid*mid > (long)x) {
+            right  = mid-1;
 
-    while(left<=right){
-        int middle = left + (right-left)/2;
-        if((long) middle*middle> (long)x){
-            right = middle -1;
+        }   else {
+            answer = mid;
+            left = mid+1;
+        }
 
-        }else if(middle*middle  == x){
-            return middle;
+    }
+
+    return answer;
+}
+
+
+
+
+
+    //     if(x == 0 || x == 1) return x;
+
+    //     int left = 1;
+    //     int right = x;
+    //     int answer = -1;
+
+    // while(left<=right){
+    //     int middle = left + (right-left)/2;
+    //     if((long) middle*middle> (long)x){
+    //         right = middle -1;
+
+    //     }else if(middle*middle  == x){
+    //         return middle;
 
         
-        }else{
-            left = middle+1;
+    //     }else{
+    //         left = middle+1;
 
-        }
-    }
-        return Math.round(right);
-    }
-    }
+    //     }
+    // }
+    //     return Math.round(right);
+    // }
+    // }
     
 
