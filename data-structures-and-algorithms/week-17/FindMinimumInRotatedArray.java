6623919f973eb4526538363149861f31bf233632
firstPush <adel.sdet2@gmail.com>
2024-11-18 19:24:49 -0600

    week 31

--- data-structures-and-algorithms/week-17/FindMinimumInRotatedArray.java
+++ data-structures-and-algorithms/week-17/FindMinimumInRotatedArray.java
@@ -8,7 +8,8 @@ public class FindMinimumInRotatedArray {
         while (left<=right) {
             int mid = left+right/2;
 
-            if (mid>0 && nums[mid-1] > nums[mid]) return mid;
+            if (mid>0 && nums[mid-1] > nums[mid]) 
+            return mid;
             if (nums[mid] > right) left = mid+1;
             else right = mid+1;
 
