968c63e94960a6c8b1263dafab6407ad25bf0f8b
firstPush <adel.sdet2@gmail.com>
2024-12-01 17:20:02 -0600

    longestCommonPrefix

--- data-structures-and-algorithms/week-14/LongestCommonPrefix.java
+++ data-structures-and-algorithms/week-14/LongestCommonPrefix.java
@@ -3,8 +3,8 @@ public class LongestCommonPrefix {
         if (str.length == 0) return "";
         String prefix = str[0];
         for (int i =1; i<str.length; i++) {
-            while (str[i].indexOf(prefix) !=0) {
-                prefix = prefix.substring(0, prefix.length()-1);
+            while (str[i].indexOf(prefix) != 0 ) {
+                prefix = prefix.substring(0, prefix.length() - 1);
 
             }
         }
