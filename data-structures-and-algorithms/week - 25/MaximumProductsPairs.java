d9d54582517b1bd549617588d8a9c402a543b3c9
firstPush <adel.sdet2@gmail.com>
2024-12-06 09:47:02 -0600

    MaximumProductsPairs

--- data-structures-and-algorithms/week - 25/MaximumProductsPairs.java
+++ data-structures-and-algorithms/week - 25/MaximumProductsPairs.java
@@ -3,6 +3,6 @@ package data-structures-and-algorithms.week - 25;
 public class MaximumProductsPairs {
     public int maxProductDifference(int[] nums) {
         int firstLargest = 0, secondLargest = 0;
+
     }
-    
-}
+    }
