9c745234f5f9638aca50e496a1b7fa281d79a907
firstPush <adel.sdet2@gmail.com>
2024-12-12 16:14:41 -0600

    rec relation

--- data-structures-and-algorithms/week - 25/SortAnArray.java
+++ data-structures-and-algorithms/week - 25/SortAnArray.java
@@ -3,8 +3,12 @@ package data-structures-and-algorithms.week - 25;
 public class SortAnArray {
     public int SortAnArray(int[] nums) {
         int n = nums.length;
-        
+        for (int i  = 0 ;i<n; i++) {
+            nums[i] +=50000;
+        }
 
+        //counting sort 
+        int k = 100000;
     }
     
 }
