9d4d9d21eff8db17c98ccb6d1b92702e72f25fd3
firstPush <adel.sdet2@gmail.com>
2024-12-26 12:48:43 -0600

    ountofSmallerNumbersAfterSelf

--- data-structures-and-algorithms/week-26/CountofSmallerNumbersAfterSelf.java
+++ data-structures-and-algorithms/week-26/CountofSmallerNumbersAfterSelf.java
@@ -10,7 +10,7 @@ public class CountofSmallerNumbersAfterSelf {
             arr[i][0] = nums[i];
             arr[i][1] = i; //original index
         }
-        
+
 
         //results[i] = count of smaller numbers a[j] after a[i] (j >i)
         List<Integer> results = new ArrayList<>();
@@ -31,8 +31,41 @@ public class CountofSmallerNumbersAfterSelf {
         //is in the left half [left; mid] and the seond is too
 
         mergeSort(arr, left, mid, results);
+        mergeSortedArray(arr, left, right, results);
+        mergeSort(arr, mid+1, right, results);
+        mergeSortedArray(arr, left, right, results);
 
 
+    }
+
+    public static void mergeSortedArray(int[][] arr, int left, int right, List<Integer> results) {
+        int len = right-left+1;
+        int[][] tmp = new int [len][2];
+        int[] updates = new int[len];
+
+        int mis = left+right/2;
+        for (int k = 0, i = left, j = mis+1; k<len; k++) {
+            if (i>n) tmp[k] = arr[j++];
+            else if (j > right) tmp[k] = arr[j++];
+            else if (arr[i][0] <= arr[j][0]) tmp[k] = arr[i++];
+            else {
+                tmp[k] = arr[j++];
+                updates[i-left] +=1;
+
+            }
+        }
+
+        int cumulative_updates = 0;
+        for (int k = 0, i = left; i<=mid; k++, i++) {
+            cumulative_updates += updates[k];
+
+            results.set(arr[i][1], results.get(arr[i][1]) + cumulative_updates);
+
+        }
+
+        for (int k = 0, i = left; k < len; k++) {
+            arr[i] = tmp[k];
+        }
     }
     
 }
