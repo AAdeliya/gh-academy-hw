29bb27b94c4484a56b3899e2a5c3245793b7cf20
firstPush <adel.sdet2@gmail.com>
2025-01-05 16:45:54 -0600

    week 14 - 3

---
+++ data-structures-and-algorithms/week-14/MergeSortedArray.java
@@ -0,0 +1,19 @@
+package data-structures-and-algorithms.week-14;
+
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums[i--];
+
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+    
+}
