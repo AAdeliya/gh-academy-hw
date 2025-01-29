d3abcff03ae83a37c72e6368e8488b5fa80af6fa
firstPush <adel.sdet2@gmail.com>
2025-01-28 19:36:53 -0600

    RemoveKDigits

--- data-structures-and-algorithms/week-28/RemoveKDigits.java
+++ data-structures-and-algorithms/week-28/RemoveKDigits.java
@@ -1,8 +1,40 @@
-package data-structures-and-algorithms.week-28;
+package data
+
+import java.util.Stack;
+
+-structures-and-algorithms.week-28;
 
 public class RemoveKDigits {
-    if (nums.length() <= k) {
+    if(nums.length()<=k)
+    {
         return "0";
+    }
+
+    // k is 0, no need of removing / performing any operations
+    if(k==0)
+    {
+        return num;
+
+    }
+
+    StringBuilder res = new StringBuilder();
+    Stack<Character> s = new Stack<>();
+
+    s.push(num.chartAt(0)); // pushing first character into stack
+
+    for(
+    int i = 1;i<nums.length();++i)
+    {
+        while (k > 0 && !s.empty() && num.charAt(i) < s.peek()) {
+            --k;
+            s.pop();
+        }
+
+        s.push(num chatAt(i));
+
+        if (s.size() == 1 && s.peek() == '0') {
+            
+        } 
     }
 
 }
