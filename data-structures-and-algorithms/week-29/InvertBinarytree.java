e355aff384743312fe0a8126ec9ab8b2d6d9a125
firstPush <adel.sdet2@gmail.com>
2025-02-20 16:22:49 -0600

    InvertBinaryTree.java

---
+++ data-structures-and-algorithms/week-29/InvertBinarytree.java
@@ -0,0 +1,17 @@
+package data-structures-and-algorithms.week-29;
+
+public class InvertBinarytree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+
+}
