423e1953f0977bc6ba25c8e97b65331f0a2c73f8
firstPush <adel.sdet2@gmail.com>
2025-02-24 21:01:36 -0600

    binary search

--- data-structures-and-algorithms/week-17/FirstBadVersion.java
+++ data-structures-and-algorithms/week-17/FirstBadVersion.java
@@ -1,18 +1,18 @@
 
-public class FirstBadVersion extends VersionControl{
-    public int FirstBadVersion(int n){
-     int left = 1;
-     int right = n;
-     int answer = -1;
+public class FirstBadVersion extends VersionControl {
+    public int FirstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        int answer = -1;
 
-     while (left<=right) {
-        int mid = left+(right-left)/2;
-        if (!isBadVersion(mid)) {
-            left = mid+1;
-        } else {
-            answer = mid;
-            right = mid+1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (!isBadVersion(mid)) {
+                left = mid + 1;
+            } else {
+                answer = mid;
+                right = mid + 1;
+            }
         }
-     }
     }
 }
