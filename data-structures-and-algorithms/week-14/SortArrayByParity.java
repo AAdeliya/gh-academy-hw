a0fb56b9c0e05d1ba1bb0f4e746152ba2d2aad29
firstPush <adel.sdet2@gmail.com>
2025-03-12 18:30:35 -0500

    sortArrayByParity

--- data-structures-and-algorithms/week-14/SortArrayByParity.java
+++ data-structures-and-algorithms/week-14/SortArrayByParity.java
@@ -1,24 +1,24 @@
 public class SortArrayByParity {
-    public int[] sortArrayByParity(int[]nums){
+    public int[] sortArrayByParity(int[] nums) {
         int i = 0;
         int j = nums.length - 1;
 
-        while(i<j){
-            if(nums[i] %2==0){
+        while (i < j) {
+            if (nums[i] % 2 == 0) {
                 i++;
-
-            }else if(nums[j] %2 !=0){
+            } else if (nums[j] % 2 != 0) {
                 j--;
-
-            }else{
+            } else {
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
-
+                i++;
+                j--;
             }
         }
+
         return nums;
-        
-     } 
-    
+
+    }
+
 }
