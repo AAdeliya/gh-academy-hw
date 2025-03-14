aa8646029bc8b2008bd3490dafea306d7873fe5b
firstPush <adel.sdet2@gmail.com>
2025-03-13 20:50:38 -0500

    week 14 - 5

--- data-structures-and-algorithms/week-14/RangeSumQuery.java
+++ data-structures-and-algorithms/week-14/RangeSumQuery.java
@@ -1,21 +1,23 @@
-public class RangeSumQuery {
-    int[] prefix; //array to store prefix sum
+public class RangeSumQuery-Immutable
+{
+    class NumArray {
+        int[] prefix; //array to store prefix sum
+    }
 
-    public void NumArray(int[]nums){
-        int n = nums.length; //length of input array 
-        prefix = new int[n+1]; //initialize the prefix sum array with size n+1
-        prefix[0] = 0; //base case : prefix sum ar index 0 is 0
-        for (int i = 1; i<=n; i++) {
-            prefix[i] = prefix[i-1] + nums[i-1]; //compute prefix sum
+    public void NumArray(int[] nums) {
+        int n = nums.length;
+        prefix = new int[n + 1];
+        prefix[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            prefix[i] = prefix[i - 1] + nums[i - 1];
 
         }
     }
 
     public int sumRange(int left, int right) {
-        left++; right++;
+        left++;
+        right++;
+        return prefix[right] - prefix[left - 1];
 
-        return prefix[right] + prefix [right-1];
     }
-    
-    }
-    
+}
