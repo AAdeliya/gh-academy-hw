208e1d881fde1fe511a769744209b6dd687834dc
firstPush <adel.sdet2@gmail.com>
2025-04-03 21:04:35 -0500

    week - 5

---
+++ data-structures-and-algorithms/week-31/AdjacencyMatrixToListUndirected5.java
@@ -0,0 +1,40 @@
+import com.sun.tools.javac.util.List;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class AdjacencyMatrixToListUndirected5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int n = in.nextInt();
+        int[][] adjacencyMatrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                adjacencyMatrix[i][j] = in.nextInt();
+            }
+        }
+        List<Pair> edges = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (adjacencyMatrix[i][j] == 1) {
+                    edges.add(new Pair(i+1, j + 1));
+                }
+            }
+            
+        }
+
+        for (Pair edge : edges) {
+            out.println(edge.u + " " + edge.v);
+        }
+
+        out.flush();
+    }
+        static class Pair {
+            int u, v;
+            public Pair(int u, int v) {
+                this.u = u;
+                this.v = v;
+            }
+        }
+    }
+    
