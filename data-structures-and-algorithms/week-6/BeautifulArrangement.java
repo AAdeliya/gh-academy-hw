6ce26247f3d17164749a911bd69ce5894c5252e2
firstPush <adel.sdet2@gmail.com>
2025-05-18 19:41:13 -0500

    week-6 1

--- data-structures-and-algorithms/week-6/BeautifulArrangement.java
+++ data-structures-and-algorithms/week-6/BeautifulArrangement.java
@@ -3,22 +3,24 @@ package data-structures-and-algorithms.week-6;
 public class BeautifulArrangement {
      int res = 0;
     public int countArrangements(int n) {
-       
         if (n == 0) return 0;
-        backtrack(1, n, new int[n+1]);
+        boolean[] used = new boolean[n + 1]; //1-index based
+        backtrack(1, n, used);
         return res;
+
     }
 
-    private void backtrack(int pos, int n, int[] usedValueArr) {
-        if (pos > n) {
+    private void backtrack(int pos, int n, boolean[] used) {
+        if (pos > n) { //base case if all positions filled
             res++;
             return;
         }
-        for (int i = 1; i <= n; i++) {
-            if (usedValueArr[i] == 0 && (pos % i == 0 || i % pos == 0)) {
-                usedValueArr[i] = 1;
-                backtrack(pos+1, n, usedValueArr);
-                usedValueArr[i] = 0;
+
+        for (int i = 1; i <=n; i++) {
+            if (!used[i] && (i % pos == 0 || pos % i == 0)) {
+                used[i] = true; //mark i as used 
+                backtrack(pos + 1, n, used);
+                used[i] = false; //mark i as unused 
             }
         }
     }
